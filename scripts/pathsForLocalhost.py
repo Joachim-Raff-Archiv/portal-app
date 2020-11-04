b65514a657546cf465166946cd2bb96f38c34678
Dennis Ried <ried-musikforschung@mail.de>
2020-11-04 17:14:24 +0100

    test2 py for serverpath changing

--- scripts/pathsForLocalhost.py
+++ scripts/pathsForLocalhost.py
@@ -1,11 +1,15 @@
+import glob
 
 collection = "/Users/dried/Repositories/JRA/portal-app/**/*.*"
 old = 'href="https://portal.raff-archiv.ch'
 new = 'href="http://localhost:8080/exist/apps/raffArchive'
 
-for inputFile in glob.iglob(collection, recursive=True): # Ich glaube das funktioniert so noch nicht, suche noch die Lösung dazu
-	with open(inputFile) as file :
-	  s = file.read()
-      s = s.replace(old, new)
-      with open(filepath, "w") as file:
-        file.write(s)
+
+
+for allfiles in glob.glob('*.*'):
+
+for inputFile in allfiles: # Ich glaube das funktioniert so noch nicht, suche noch die Lösung dazu
+	with open(inputFile, 'w') as file :
+	  filedata = file.read() # Daten aus Datei in variable lesen
+	  replacement = filedata.replace(old, new) # String korrigieren
+	  file.write(replacement) # Korrigierten String in die Datei schreiben
