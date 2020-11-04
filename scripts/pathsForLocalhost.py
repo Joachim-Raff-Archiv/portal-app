d940e744ee298ff1dfac7abb485788ccce94fe58
Dennis Ried <ried-musikforschung@mail.de>
2020-11-04 17:21:42 +0100

    test3 py for serverpath changing

--- scripts/pathsForLocalhost.py
+++ scripts/pathsForLocalhost.py
@@ -1,15 +1,18 @@
-import glob
+
+import os, fnmatch
+
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
 
 collection = "/Users/dried/Repositories/JRA/portal-app/**/*.*"
 old = 'href="https://portal.raff-archiv.ch'
 new = 'href="http://localhost:8080/exist/apps/raffArchive'
 
-
-
-for allfiles in glob.glob('*.*'):
-
-for inputFile in allfiles: # Ich glaube das funktioniert so noch nicht, suche noch die Lösung dazu
-	with open(inputFile, 'w') as file :
-	  filedata = file.read() # Daten aus Datei in variable lesen
-	  replacement = filedata.replace(old, new) # String korrigieren
-	  file.write(replacement) # Korrigierten String in die Datei schreiben
+findReplace(collection, old, new, "*.*")
