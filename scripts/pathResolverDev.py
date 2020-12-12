f07eea62356a226c72951656fd48e53759fa1614
Dennis Ried <ried-musikforschung@mail.de>
2020-12-12 13:47:45 +0100

    fix resolver paths

--- scripts/pathResolverDev.py
+++ scripts/pathResolverDev.py
@@ -13,7 +13,7 @@ def findReplace(directory, find, replace, filePattern):
 
 collection = "/Users/dried/Repositories/JRA/portal-app"
 
-old = 'http://localhost:8080/exist/apps/raffArchive'
+old = 'https://portal.raff-archiv.ch'
 new = 'https://dev.raff-archiv.ch'
 
 findReplace(collection, old, new, "*.*")
